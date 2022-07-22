a7d94965d5858884eca5f5354c02e1873a3b7b9b
Julia Dubas <uliadubas94@gmail.com>
2022-07-22 13:26:58 +0300

    Add project files.

---
+++ color ghost/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace color_ghost
+{
+    class Chost
+    {
+        string[] colors = new string[] { "white", "yellow", "purple", "red" };
+
+        public string color;
+
+        public  Chost()
+        {
+            Random rnd = new Random();
+
+            int index = rnd.Next(colors.Length);
+
+            color = colors[index];
+        }
+        
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Chost chost = new Chost();
+
+            Console.WriteLine(chost.color);
+
+            Console.ReadLine();
+        }
+    }
+}
